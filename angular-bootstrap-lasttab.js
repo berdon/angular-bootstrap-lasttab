b571266f4bdf265fc24c5635df7d245ceeb2220c
Austin Hanson <berdon@gmail.com>
2015-11-23 10:15:38 -0600

    Fixing compilation issue

--- angular-bootstrap-lasttab.js
+++ angular-bootstrap-lasttab.js
@@ -4,7 +4,7 @@ angular.module('angular-bootstrap-lasttab', [ ]).
 	provider('$lastTab', function $lastTab() {
 		function setupTabs() {
 			// 'shown.bs.tab' for Bootstrap 3, 'shown' for Bootstrap 2
-		    $('a[data-toggle="tab"]').on('shown.bs.tab shown', function {
+		    $('a[data-toggle="tab"]').on('shown.bs.tab shown', function() {
 				// save the latest tab; use cookies if you like 'em better:
 		        localStorage.setItem('lastTab', $(this).attr('data-target'));
 			});
