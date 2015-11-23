1d654930b9873d02c6e5668629187bc315bc26c9
Austin Hanson <berdon@gmail.com>
2015-11-23 09:33:05 -0600

    Initial commit of base angular-bootstrap-lasttab file.

---
+++ angular-bootstrap-lasttab.js
@@ -0,0 +1,27 @@
+(function(window, angular, undefined) {'use strict';
+
+angular.module('angular-bootstrap-lasttab', [ ]).
+	provider('$lastTab', function $lastTab() {
+		function setupTabs() {
+			// 'shown.bs.tab' for Bootstrap 3, 'shown' for Bootstrap 2
+		    $('a[data-toggle="tab"]').on('shown.bs.tab shown', function {
+				// save the latest tab; use cookies if you like 'em better:
+		        localStorage.setItem('lastTab', $(this).attr('data-target'));
+			});
+
+		    // go to the latest tab, if it exists:
+		    var lastTab = localStorage.getItem('lastTab');
+		    if (lastTab) {
+		        $('[data-target="' + lastTab + '"]').tab('show');
+		    }
+		}
+
+		this.$get = function($rootScope) {
+			$rootScope.$on('$viewContentLoaded', setupTabs);
+
+			return {
+				init: setupTabs
+			};
+		};
+	});
+})(window, window.angular);
